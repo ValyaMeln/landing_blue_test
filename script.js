9f8a9f1b30fce711246389f78acfa1b74e2fc306
Valya Melnychuk <valyameln@gmail.com>
2021-07-21 19:27:36 +0300

    js

--- script.js
+++ script.js
@@ -6,15 +6,27 @@ for (let li of lishku) {  /*перебираєм усі лішки*/
         function () {
 
             for (let lii of lishku) {      //перебираєм ті самі лішки, забираючи в кожої клас action
+                if (this === lii && !this.classList.contains("active")) {
+                    this.classList.add("active")
+                    break
+                }
                 lii.classList.remove("active")
+
             }
-            this.classList.add("active")  //додаємо клас action, на ту лішку на яку ми нажали (на ту яка запустила подію)
+            // this.classList.add("active")
+            //додаємо клас action, на ту лішку на яку ми нажали (на ту яка запустила подію)
+            /*  this ___ на ту лішку на яку ми нажали*/
 
-            let x = this.getAttribute('img-src') //створюємо зміну і посилаємся на ту ж саму лішку зі значення атрибута по назві 'img-src'
-            let imageSrc = `img_three/` + x // до рядка додаємо значення атрибута
-            let image = document.querySelector('#image') //знаходе в html селекор id='image'
+            // if (!this.classList.contains("active")){ //* перевіряємо чи є клас active *//
+            //     this.classList.remove("active")//* якщо є клас active то ми видаляємо, якщо ні то..*//
+            // }
+            // else {this.classList.add("active")}
 
-            image.setAttribute("src", imageSrc) //вказуємо нове значення атрибута "src"
+            // let x = this.getAttribute('img-src') //створюємо зміну і посилаємся на ту ж саму лішку зі значення атрибута по назві 'img-src'
+            // let imageSrc = `img_three/` + x // до рядка додаємо значення атрибута
+            // let image = document.querySelector('#image') //знаходе в html селекор id='image'
+            //
+            // image.setAttribute("src", imageSrc) //вказуємо нове значення атрибута "src"
         })
 
 }
