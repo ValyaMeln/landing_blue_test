fc0d0672736ead0a3b850b81997b789ab6c80050
Valya Melnychuk <valyameln@gmail.com>
2021-07-21 20:02:22 +0300

    додала background кнопкам

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let lishku = document.querySelectorAll(".be_good li")
-// console.log(lishku);
+
 for (let li of lishku) {  /*перебираєм усі лішки*/
     console.log(li);
     li.addEventListener("click", /*підписуємся на подію клік по черзі кожній*/
@@ -11,22 +11,6 @@ for (let li of lishku) {  /*перебираєм усі лішки*/
                     continue       //якщо ми не зупинили цикл то переходимо до наступної дії, а саме видаляємо клас "active"
                 }
                 lii.classList.remove("active")
-
             }
-            // this.classList.add("active")
-            //додаємо клас action, на ту лішку на яку ми нажали (на ту яка запустила подію)
-            /*  this ___ на ту лішку на яку ми нажали*/
-
-            // if (!this.classList.contains("active")){ //* перевіряємо чи є клас active *//
-            //     this.classList.remove("active")//* якщо є клас active то ми видаляємо, якщо ні то..*//
-            // }
-            // else {this.classList.add("active")}
-
-            // let x = this.getAttribute('img-src') //створюємо зміну і посилаємся на ту ж саму лішку зі значення атрибута по назві 'img-src'
-            // let imageSrc = `img_three/` + x // до рядка додаємо значення атрибута
-            // let image = document.querySelector('#image') //знаходе в html селекор id='image'
-            //
-            // image.setAttribute("src", imageSrc) //вказуємо нове значення атрибута "src"
         })
-
 }
