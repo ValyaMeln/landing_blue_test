6e72943c1ff97c8e8e641f31c8f10a82422a6094
Valya Melnychuk <valyameln@gmail.com>
2021-07-21 19:42:18 +0300

    js_2

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ for (let li of lishku) {  /*перебираєм усі лішки*/
         function () {
 
             for (let lii of lishku) {      //перебираєм ті самі лішки, забираючи в кожої клас action
-                if (this === lii && !this.classList.contains("active")) {
-                    this.classList.add("active")
-                    break
+                if (this === lii && !this.classList.contains("active")) { //якщо ми нажавши на liі отримаємо, що вона дорівнює liі "і" не має класу "active" →
+                    this.classList.add("active") //→ то ми до неї додаємо клас "active" і зупиняємо дію  "continue" переходячи на ноступну lii y for//
+                    continue       //якщо ми не зупинили цикл то переходимо до наступної дії, а саме видаляємо клас "active"
                 }
                 lii.classList.remove("active")
 
